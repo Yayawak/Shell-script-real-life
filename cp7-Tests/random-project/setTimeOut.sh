f8d5c1dd63ddf522cef4a1009c205b30f8cb2265
Elio <elio.interesante@gmail.com>
2022-04-08 00:58:30 +0700

    added random-project to git

---
+++ cp7-Tests/random-project/setTimeOut.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TMOUT=3
+echo "What is your favorite song?"
+echo "Quickly now, you only have $TMOUT seconds to answer!"
+read song
+
+if [ -z "$song" ]
+then
+  song="(no answer)"
+  # Default response.
+fi
+
+echo "Your favorite song is $song."
